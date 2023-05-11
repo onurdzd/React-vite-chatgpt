94e3382f8c49ca781486a8b937b0234f5fcfb992
onurd <106928484+onurdzd@users.noreply.github.com>
2023-05-11 16:02:33 +0300

    vercel post req added

--- gpt test/src/App.jsx
+++ gpt test/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { PORT } from "../config/config";
+// import { PORT } from "../config/config";
 import { TiWeatherNight } from "react-icons/ti";
 import { BsSun } from "react-icons/bs";
 
@@ -11,7 +11,7 @@ function App() {
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem("theme"))
   );
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
 
   const localTheme = () => {
     localStorage.setItem("theme", JSON.stringify(!darkMode));
@@ -22,21 +22,83 @@ function App() {
   };
 
   const request = async () => {
+    //localhost dan axios:
+    // await axios
+    //   .post(
+    //     `http://localhost:${PORT}/gpt`,
+    //     { icerik: `${inputData}` },
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     setLoading(false)
+    //     setData(res.data.message);
+    //   })
+    //   .catch((err) =>{setLoading(false);setData("Kullanım hakkın doldu.Admin ile iletişime geç"); console.log(err)});
+
+    //vercel için axios:
+
+    // let options = {
+    //   method: 'POST',
+    //   url: import.meta.env.VITE_URL,
+    //   headers: {
+    //     'content-type': 'application/json',
+    //     'X-RapidAPI-Key': import.meta.env.VITE_OPENAI_API_KEY,
+    //     'X-RapidAPI-Host': import.meta.env.VITE_HOST
+    //   },
+    //   data: {
+    //     model: 'gpt-3.5-turbo',
+    //     messages: [
+    //       {
+    //         role: 'user',
+    //         content: inputData
+    //       }
+    //     ]
+    //   }
+    // };
+
+    // const response = await axios.request(options);
+    // console.log("akış içinde",response);
+    // if (response) {
+    //   setLoading(false);
+    //   setData(response);
+    // } else {
+    //   setLoading(false);
+    //   setData("Kullanım hakkın doldu.Admin ile iletişime geç");
+    // }
+
     await axios
       .post(
-        `http://localhost:${PORT}/gpt`,
-        { icerik: `${inputData}` },
+        `${import.meta.env.VITE_URL}`,
         {
           headers: {
-            "Content-Type": "application/json",
+            "content-type": "application/json",
+            "X-RapidAPI-Key": import.meta.env.VITE_OPENAI_API_KEY,
+            "X-RapidAPI-Host": import.meta.env.VITE_HOST,
+          },
+          data: {
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: inputData,
+              },
+            ],
           },
         }
       )
       .then((res) => {
-        setLoading(false)
+        setLoading(false);
         setData(res.data.message);
       })
-      .catch((err) =>{setLoading(false);setData("Kullanım hakkın doldu.Admin ile iletişime geç"); console.log(err)});
+      .catch((err) => {
+        setLoading(false);
+        setData("Kullanım hakkın doldu.Admin ile iletişime geç");
+        console.log(err);
+      });
   };
 
   return (
@@ -69,7 +131,9 @@ function App() {
             <h1 className="text-orange-400 font-bold text-xl mb-2">
               Chat GPT Test
             </h1>
-            <h3 className="mb-3 font-medium">Chat GPT Api test için hazırlandı</h3>
+            <h3 className="mb-3 font-medium">
+              Chat GPT Api test için hazırlandı
+            </h3>
             <div className="flex gap-2 justify-center">
               <input
                 className="rounded-md p-2 border-gray-950 border-2  font-medium text-sky-500"
@@ -77,12 +141,17 @@ function App() {
               ></input>
               <button
                 className="bg-slate-600 text-orange-400 rounded-md pl-3 pr-3 font-bold text-lg"
-                onClick={()=>{request();setLoading(true)}}
+                onClick={() => {
+                  request();
+                  setLoading(true);
+                }}
               >
-               Sor
+                Sor
               </button>
             </div>
-            <div className="mt-5 p-5 w-1/2 text-xl m-auto text-center font-medium">{loading == true ? "Sorunu araştırıyorum..." : data}</div>
+            <div className="mt-5 p-5 w-1/2 text-xl m-auto text-center font-medium">
+              {loading == true ? "Sorunu araştırıyorum..." : data}
+            </div>
           </div>
         </div>
       </div>
